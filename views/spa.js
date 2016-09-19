a2847ac8241d79106c37ea6d45f2645144097415
Soundararajan <soundararajan@usa.net>
2016-09-19 13:17:42 -0700

    edit removed.

--- views/spa.js
+++ views/spa.js
@@ -2,7 +2,7 @@ const Closeout = React.createClass({
   getInitialState: function(){
      var today = getToday();
      var store = Data[0].name;
-     console.log("store = " + store);
+     //console.log("store = " + store);
      return {
        selectedStore: store,
        posted: -999.99,
@@ -36,6 +36,13 @@ const Closeout = React.createClass({
      this.state.notes = notes;
   },
   validateEntries: function(){
+    var duplicateRecord = this.doesExist(this.state.selectedStore, this.state.date);
+    // console.log("duplicateRecord = " + duplicateRecord + " this.state.date = " + this.state.date + " this.state.posted = "+ this.state.posted);
+    // console.log("printArr(this.state.denominations) = " + printArr(this.state.denominations));
+    if(duplicateRecord){
+      //alert("Duplicate record");
+      return false;
+    }
     var retValue = true;
     if(this.state.date !== '' && this.state.posted >= 0.00 && this.state.denominations.length > 0
        && this.state.selectedStore !== 'Select'){
@@ -47,12 +54,26 @@ const Closeout = React.createClass({
     }
     return retValue;
   },
+  doesExist: function(store, date){
+     var retVal = false;
+     //console.log("inside doesExist store = " + store + "  Date = " + date);
+     this.state.closeoutRows.forEach( (item) => {
+       //console.log("inside forEach item.store = " + item.store + "  item.Date = " + item.date);
+       if(item.store === store && item.date === date){
+         retVal = true;
+         //break;
+       }
+     
+    });
+     return retVal;
+  },
   clearEntries: function(){
      var store = Data[0].name;
      //console.log("clearing the entries " + store);
      this.myCloseoutDateInput.handleSubmit();
      this.myPostedCloseoutInput.handleSubmit();
      this.myDenominations.handleSubmit();
+     this.myNotesText.handleSubmit();
      this.setState({
        selectedStore: store,
        posted: -999.99,
@@ -70,27 +91,57 @@ const Closeout = React.createClass({
        notes: '',
      });
   },
-  submitClickHandler: function(){
+  submitClickHandler: function(doNotClear){
     if(this.validateEntries() === true){
-      console.log("submitClickHandler => " + printArr(this.state.denominations));
+      //console.log("submitClickHandler => " + printArr(this.state.denominations));
       var ttt = printArr(this.state.denominations);
       var actual = getSum(ttt);
       var temp = {store: this.state.selectedStore, posted: this.state.posted, denominations: this.state.denominations};
+      //console.log("at submitClickHandler, temp.store = " + temp.store, " temp.posted = " + temp.posted );
+
       this.setState( { closeoutCurrentRow: temp} );
+
+
+         //console.log(this.state.closeoutCurrentRow);
+
+      //console.log("at submitClickHandler  end");
+
       var temp2 = {id: (this.state.closeoutRows.length + 1), store: this.state.selectedStore, date: this.state.date, posted: this.state.posted, denominations: ttt, actual: actual, notes: this.state.notes};
       this.state.closeoutRows.push(temp2);
       this.clearEntries();
+
     }else{
-      alert("Invalid " + this.state.date);
+      alert("Please fill out all the required fields. Check for Duplicate.");
     }
   },
   changeStoreHandler: function(event){
     this.setState({ selectedStore: event.target.value});
   },
   denominationsChangeHandler: function(denoms){
-     console.log("denominationsChangeHandler => " + printArr(denoms));
+     //console.log("denominationsChangeHandler => " + printArr(denoms));
      this.setState( {denominations: denoms });
   },
+  buttonFunction: function(cell, row) {      
+  return <label>
+            <button type="button" 
+                    onClick={() => {this.deleteRow(row)}} 
+                    className="bbtn btn-danger btn-sm glyphicon glyphicon-remove">
+                      
+            </button>
+         </label>        
+ },
+  deleteRow: function(row){
+     var newCloseOutRows = [];
+     this.state.closeoutRows.forEach( (item) => {
+       if(item.store === row.store && item.date === row.date){
+         
+       }else{
+        newCloseOutRows.push(item);
+       }
+    });
+    this.setState({closeoutRows: newCloseOutRows});
+  },
+
   render: function(){
     const stores = Data.map( (store) => {
            return(
@@ -111,7 +162,7 @@ const Closeout = React.createClass({
                 </div>
               </div>
               <div className="col-md-3">
-                 <CloseoutDateInput label="CloseoutDate" value={this.state.date} onChange={this.closeoutDateChangeHandler} ref={(ref) => this.myCloseoutDateInput = ref}/>
+                 <CloseoutDateInput label="CloseoutDate" placeholder="Enter Date" size="11" value={this.state.date} onChange={this.closeoutDateChangeHandler} ref={(ref) => this.myCloseoutDateInput = ref}/>
               </div>
               <div className="col-md-3">
                   <PostedCloseoutInput label="Posted:" onChange={this.postedChangeHandler} ref={(ref) => this.myPostedCloseoutInput = ref}/>
@@ -124,7 +175,7 @@ const Closeout = React.createClass({
               <button className="btn btn-danger" onClick={this.submitClickHandler}>Submit</button>
           </div> 
           <br />
-          <CloseoutDateInput label="Notes:"  onChange={this.notesChangeHandler}/>
+          <CloseoutDateInput label="Notes:" size="100"  placeholder="Enter Notes"  onChange={this.notesChangeHandler} ref={(ref) => this.myNotesText = ref}/>
           <hr />
           <PostedCloseoutInput label="FileName:" onChange={this.filenameChangeHandler}/>
           <BootstrapTable data={this.state.closeoutRows} striped={true} hover={true} exportCSV={true} csvFileName={this.state.filename}>
@@ -135,6 +186,7 @@ const Closeout = React.createClass({
             <TableHeaderColumn dataField="actual" isKey={false} >Counted</TableHeaderColumn>
             <TableHeaderColumn dataField="denominations" isKey={false}>Denominations</TableHeaderColumn>
             <TableHeaderColumn dataField="notes" isKey={false}>Notes</TableHeaderColumn>
+            <TableHeaderColumn  isKey={false} dataFormat={this.buttonFunction} >Edit/Delete</TableHeaderColumn>
          </BootstrapTable>     
         </div>
       );
@@ -176,9 +228,26 @@ const getToday = function (){
   return today;
 }
 
+const toDenoms = function(denoms){
+  // console.log("at toDenoms denoms = " + denoms);
+  var arr = denoms.split("]");
+  var newDenoms = [];
+  arr.forEach( (item) => {
+    var a = item.split(",");
+    if(a.length > 1)
+       newDenoms.push({qty: a[1], currency:  a[0], product: a[2]});
+  });
+  // console.log("at toDenoms printing the returning array");
+  // newDenoms.forEach( (item) => {
+  //   console.log(item);
+  // });
+  // console.log("at toDenoms printing the returning array end");
+  return newDenoms;
+
+}
 const Denominations = React.createClass({
     handleSubmit: function(){
-      console.log("handleSubmit of Denominations");
+      //console.log("handleSubmit of Denominations");
       this.setState({denominations: [{qty: 0, currency: 100, product: 0.00}, 
                          {qty: 0, currency:  50, product: 0.00}, 
                          {qty: 0, currency:  20, product: 0.00}, 
@@ -192,14 +261,15 @@ const Denominations = React.createClass({
       //alert(event.target.value);
       var total = 0.00;
       var newdenoms = this.state.denominations.map( (item)  => {
-
-         if(item.currency == event.target.id && event.target.value >= 0){
-           //alert(item.currency + " == " + event.target.id); 
-           item.qty = event.target.value;
-           
+          if(item.product !== undefined){
+             if(item.currency == event.target.id && event.target.value >= 0){
+               //alert(item.currency + " == " + event.target.id); 
+               item.qty = event.target.value;
+               
+             }
+             item.product = item.qty * item.currency;
+             total += parseInt(item.product);
          }
-         item.product = item.qty * item.currency;
-         total += item.product;
          return item;
       });
       this.setState({denominations: newdenoms, total: total});
@@ -220,49 +290,49 @@ const Denominations = React.createClass({
                     <div className="col-sm-1">X</div>
                     <div className="col-sm-3"><label>{this.state.denominations[0].currency} </label></div>
                     <div className="col-sm-1">=</div>
-                    <div className="col-sm-4"><input type="number" value={this.state.denominations[0].product} /></div>
+                    <div className="col-sm-4"><label>{this.state.denominations[0].product} </label></div>
                 </div>
                 <div className="well">
                     <div className="col-sm-3"><input type="number" id={this.state.denominations[1].currency} value={this.state.denominations[1].qty} onChange={this.handleQtyChange}/></div>
                     <div className="col-sm-1">X</div>
-                    <div className="col-sm-3"><input type="number" value={this.state.denominations[1].currency} /></div>
+                    <div className="col-sm-3"><label>{this.state.denominations[1].currency} </label></div>
                     <div className="col-sm-1">=</div>
-                    <div className="col-sm-4"><input type="number" value={this.state.denominations[1].product} /></div>
+                    <div className="col-sm-4"><label>{this.state.denominations[1].product} </label></div>
                 </div>
                 <div className="well">
                     <div className="col-sm-3"><input type="number" id={this.state.denominations[2].currency} value={this.state.denominations[2].qty} onChange={this.handleQtyChange}/></div>
                     <div className="col-sm-1">X</div>
-                    <div className="col-sm-3"><input type="number" value={this.state.denominations[2].currency} /></div>
+                    <div className="col-sm-3"><label>{this.state.denominations[2].currency} </label></div>
                     <div className="col-sm-1">=</div>
-                    <div className="col-sm-4"><input type="number" value={this.state.denominations[2].product} /></div>
+                    <div className="col-sm-4"><label>{this.state.denominations[2].product} </label></div>
                 </div>
                 <div className="well">
                     <div className="col-sm-3"><input type="number" id={this.state.denominations[3].currency} value={this.state.denominations[3].qty} onChange={this.handleQtyChange}/></div>
                     <div className="col-sm-1">X</div>
-                    <div className="col-sm-3"><input type="number" value={this.state.denominations[3].currency} /></div>
+                    <div className="col-sm-3"><label>{this.state.denominations[3].currency} </label></div>
                     <div className="col-sm-1">=</div>
-                    <div className="col-sm-4"><input type="number" value={this.state.denominations[3].product} /></div>
+                    <div className="col-sm-4"><label>{this.state.denominations[3].product} </label></div>
                 </div>
                 <div className="well">
                     <div className="col-sm-3"><input type="number" id={this.state.denominations[4].currency} value={this.state.denominations[4].qty} onChange={this.handleQtyChange}/></div>
                     <div className="col-sm-1">X</div>
-                    <div className="col-sm-3"><input type="number" value={this.state.denominations[4].currency} /></div>
+                    <div className="col-sm-3"><label>{this.state.denominations[4].currency} </label></div>
                     <div className="col-sm-1">=</div>
-                    <div className="col-sm-4"><input type="number" value={this.state.denominations[4].product} /></div>
+                    <div className="col-sm-4"><label>{this.state.denominations[4].product} </label></div>
                 </div>
                 <div className="well">
                     <div className="col-sm-3"><input type="number" id={this.state.denominations[5].currency} value={this.state.denominations[5].qty} onChange={this.handleQtyChange}/></div>
                     <div className="col-sm-1">X</div>
-                    <div className="col-sm-3"><input type="number" value={this.state.denominations[5].currency} /></div>
+                    <div className="col-sm-3"><label>{this.state.denominations[5].currency} </label></div>
                     <div className="col-sm-1">=</div>
-                    <div className="col-sm-4"><input type="number" value={this.state.denominations[5].product} /></div>
+                    <div className="col-sm-4"><label>{this.state.denominations[5].product} </label></div>
                 </div>
                 <div className="well">
                     <div className="col-sm-3"><input type="number" id={this.state.denominations[6].currency} value={this.state.denominations[6].qty} onChange={this.handleQtyChange}/></div>
                     <div className="col-sm-1">X</div>
-                    <div className="col-sm-3"><input type="number" value={this.state.denominations[6].currency} /></div>
+                    <div className="col-sm-3"><label>{this.state.denominations[6].currency} </label></div>
                     <div className="col-sm-1">=</div>
-                    <div className="col-sm-4"><input type="number" value={this.state.denominations[6].product} /></div>
+                    <div className="col-sm-4"><label>{this.state.denominations[6].product} </label></div>
                 </div>
              </div> 
              <div className="row">
@@ -347,7 +417,7 @@ const CloseoutDateInput = React.createClass({
         return(
             <div class="input-group">
                 <span class="input-group-addon">{this.props.label}</span>
-                <input type="text" class="form-control" placeholder="close out date"  value={this.state.Date} onChange={this.handleChange} />
+                <input type="text" class="form-control" size={this.props.size} placeholder={this.props.placeholder}  value={this.state.Date} onChange={this.handleChange} />
             </div>
         );
     },
@@ -360,8 +430,5 @@ var closeoutInstance = ReactDOM.render(
     document.getElementById('content')
 );
 
-// ReactDOM.render(
-//     <Denominations />,
-//     document.getElementById('denoms')
-// );
+
 
